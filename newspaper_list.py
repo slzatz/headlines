8ac7593b34d9521f502f333cad554ca3fadd5b97
slzatz <slzatz@gmail.com>
2024-02-05 13:14:36 +0000

    added newspaper_list.py

---
+++ newspaper_list.py
@@ -0,0 +1,44 @@
+newspapers = [
+#"new-york-post", # us
+"the-new-york-times", # us
+"the-wall-street-journal", # us
+"the-washington-post", # us
+"los-angeles-times", # us
+"the-boston-globe", # us
+#"the-washington-times", # us
+"usa-today", # us
+"le-monde", # france
+"le-figaro", # france
+"le-parisien", # france
+"la-croix", # france
+"the-guardian", # uk
+"the-daily-telegraph", # uk 
+"the-i-paper", # uk
+#"daily-mail", # uk #tabloid
+"financial-times", # uk
+"the-times", # uk #tabloid
+#"the-sun", # uk #tabloid
+#"daily-mirror", # uk #tabloid
+#"daily-express", # uk #tabloid
+"bild", # germany
+"suddeutsche-zeitung", # germany
+"die-welt", # germany
+"el-pais", # spain
+"el-mundo", # spain
+"la-vanguardia", # spain
+"abc-es", # spain # a little farther down the list
+"haaretz", # israel
+"the-jerusalem-post", # israel
+"de-telegraaf", # netherlands
+"yomiuri-shimbun", # japan
+"mainichi-shimbun", # japan
+#"asahi-shimbun", # japan #not in list
+"people-s-daily", # china
+"herald-sun", # australia
+"corriere-della-sera", # italy
+"la-repubblica", # italy
+"la-stampa", # italy
+"neue-zurcher-zeitung", # switzerland
+"the-globe-and-mail", # canada
+"kronen-zeitung", # austria
+]
