dcf27b2c98e159c8161ff38e460dcd78c5701e1a
slzatz <slzatz@gmail.com>
2026-01-19 11:41:42 -0500

    updated newspaper list

--- newspaper_list.py
+++ newspaper_list.py
@@ -1,49 +1,13 @@
 newspapers = [
 "new-york-post", # us
 "the-new-york-times", # us
-#"the-wall-street-journal", # us
 "the-washington-post", # us
 "los-angeles-times", # us
 "the-boston-globe", # us
 "chicago-tribune", # us
-#"the-washington-times", # us
 "houston-chronicle", # us
-"usa-today", # us
-"le-monde", # france
-"le-figaro", # france
-"le-parisien", # france
-"la-croix", # france
-"the-guardian-usa", # uk
-"the-daily-telegraph", # uk 
-"the-i-paper", # uk
-#"daily-mail", # uk #tabloid
 "financial-times", # uk
-"the-times", # uk #tabloid
-#"the-sun", # uk #tabloid
-#"daily-mirror", # uk #tabloid
-#"daily-express", # uk #tabloid
-#"bild", # germany tabloid
-"der-tagesspiegel", # germany
-"frankfurter-allgemeine-zeitung", # germany
-"suddeutsche-zeitung", # germany
-"die-welt", # germany
-"el-pais", # spain
-"el-mundo", # spain
-"la-vanguardia", # spain
-"abc-es", # spain # a little farther down the list
 "haaretz", # israel
 "the-jerusalem-post", # israel
-"de-telegraaf", # netherlands
-"yomiuri-shinbun", # japan
-"mainichi-shinbun", # japan
-#"asahi-shimbun", # japan #not in list
-"people-s-daily", # china
-"herald-sun", # australia
-"corriere-della-sera", # italy
-"la-repubblica", # italy
-"la-stampa", # italy
-"neue-zurcher-zeitung", # switzerland
 "the-globe-and-mail", # canada
-"die-presse", # austria
-#"kronen-zeitung", # austria #tabloid
 ]
